8d7332522ad53fc217a80c826953cff13685554c
Rexford Asamoah <36260221+rexthecoder@users.noreply.github.com>
2022-07-12 22:42:34 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is RextheBot🔥
+My Creator is rexthecoder😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+233544608462'] //Owner number in aive msg
 global.premium = ['+233544608462'] //Owner Number info
